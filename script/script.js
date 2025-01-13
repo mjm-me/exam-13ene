dc247d5ba9ddc5e0213b48595623cd997652859d
mj <mjmartinsu@yahoo.es>
2025-01-13 13:47:45 +0100

    Add JS for buttons

---
+++ script/script.js
@@ -0,0 +1,28 @@
+document
+  .getElementById("button-send")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const opinion1 = document.getElementById("opinion1").value;
+    const opinion2 = document.getElementById("opinion2").value;
+    const opinion3 = document.getElementById("opinion3").value;
+    const opinion4 = document.getElementById("opinion4").value;
+    const opinion5 = document.getElementById("opinion5").value;
+
+    document.getElementById("containerGlobal").classList.add("hidden");
+    document.getElementById("containerSencondary").classList.remove("hidden");
+
+    showOpinion();
+  });
+
+const box = document.querySelectorAll("#containerGlobal");
+
+function showOpinion() {
+  if (div.containerGlobal === "abierto") {
+    div.containerGlobal = "";
+  }
+}
+
+document.querySelectorAll(".button-send").forEach((button) => {
+  button.addEventListener("click", showOpinion());
+});
