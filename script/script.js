1fc65fbe96921210ef049df1673d17f0455d114d
mj <mjmartinsu@yahoo.es>
2025-01-13 13:56:39 +0100

    Upload JS

--- script/script.js
+++ script/script.js
@@ -1,3 +1,35 @@
+ var opinion1 = document.getElementById('opinion1');
+        var opinion2 = document.getElementById('opinion2');
+        var opinion3 = document.getElementById('opinion3');
+                var opinion4 = document.getElementById('opinion4');
+                        var opinion5 = document.getElementById('opinion5');
+        var clickCount = 0;
+
+        var clickRecordsContainer = document.getElementById('clickRecords');
+        boton1.addEventListener('click', function () {
+            clickCount++;
+            var clickDateTime = new Date();
+            var newRecord = document.createElement('p');
+            newRecord.textContent = 'Clic ' + clickCount + ': ' + txtboton1 + '. ' + clickDateTime.toString();
+            clickRecordsContainer.appendChild(newRecord);
+        });
+        boton2.addEventListener('click', function () {
+            clickCount++;
+            var clickDateTime = new Date();
+            var newRecord = document.createElement('p');
+            newRecord.textContent = 'Clic ' + clickCount + ': ' + txtboton2 + '. ' + clickDateTime.toString();
+            clickRecordsContainer.appendChild(newRecord);
+        });
+        boton3.addEventListener('click', function () {
+            clickCount++;
+            var clickDateTime = new Date();
+            var newRecord = document.createElement('p');
+            newRecord.textContent = 'Clic ' + clickCount + ': ' + txtboton3 + '. ' + clickDateTime.toString();
+            clickRecordsContainer.appendChild(newRecord);
+        });
+    </script>
+
+
 document;
 buttonSend.getElementById("button-send");
 buttonSend.addEventListener("submit", function (event) {
