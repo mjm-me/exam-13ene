382b1c810ca8401c99cc2872cab767c80907d73b
mj <mjmartinsu@yahoo.es>
2025-01-13 13:52:35 +0100

    Upload JS

--- script/script.js
+++ script/script.js
@@ -1,19 +1,19 @@
-document
-  .getElementById("button-send")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+document;
+buttonSend.getElementById("button-send");
+buttonSend.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    const opinion1 = document.getElementById("opinion1").value;
-    const opinion2 = document.getElementById("opinion2").value;
-    const opinion3 = document.getElementById("opinion3").value;
-    const opinion4 = document.getElementById("opinion4").value;
-    const opinion5 = document.getElementById("opinion5").value;
+  const opinion1 = document.getElementById("opinion1").value;
+  const opinion2 = document.getElementById("opinion2").value;
+  const opinion3 = document.getElementById("opinion3").value;
+  const opinion4 = document.getElementById("opinion4").value;
+  const opinion5 = document.getElementById("opinion5").value;
 
-    document.getElementById("containerGlobal").classList.add("hidden");
-    document.getElementById("containerSencondary").classList.remove("hidden");
+  document.getElementById("containerGlobal").classList.add("hidden");
+  document.getElementById("containerSencondary").classList.remove("hidden");
 
-    showOpinion();
-  });
+  showOpinion();
+});
 
 const box = document.querySelectorAll("#containerGlobal");
 
